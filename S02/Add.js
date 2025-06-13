6740e8c5281ee89b82f180856c79940757606732
Kabir <sagithedeveloper@gmail.com>
2025-06-13 07:58:34 +0530

    Added the code

---
+++ S02/Add.js
@@ -0,0 +1,13 @@
+function fnAdd(x, y) {
+  //by default function will return undefined value
+  var num1 = x;
+
+  var num2 = y;
+
+  var result = num1 + num2;
+
+  console.log(result);
+}
+
+var response = fnAdd(20, 30);
+console.log(response);
---
+++ S02/object.js
@@ -0,0 +1,8 @@
+var user = {
+  name: "sagar",
+  id: 101,
+  email: "sagar@gmail.com",
+  gender: "male",
+  
+};
+
